c937f899c7ab535afee1d6fc737687a565eaa4ab
sebasbl506 <sebasbl1506@gmail.com>
2025-06-09 03:51:01 -0500

    Delete iniciar_pro.sh

--- iniciar_pro.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "🚀 Iniciando GROWTH MASTER PRO VIP..."
-pkill -f control_vip.py
-nohup python control_vip.py > logs.txt 2>&1 &
-echo "✅ Bot iniciado en segundo plano correctamente."
